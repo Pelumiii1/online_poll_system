3e7addbd048a15482b97b8575596843a7ca7de96
Pelumiii1 <adesokanpelumi14@gmail.com>
2025-09-17 15:02:30 +0100

    set up django with login and registeration, added postgresql and added jwt for token and authentication

---
+++ online_poll_system/accounts/views.py
@@ -0,0 +1,51 @@
+from rest_framework.views import APIView
+from .serializer import RegisterSerializer,LoginSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth import authenticate
+from rest_framework_simplejwt.tokens import RefreshToken
+
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        
+        if serializer.is_valid():
+            user = serializer.save()
+            
+            return Response({
+                "message" : "User registered successfully",
+                "user":{
+                    "id":user.id,
+                    "first_name": user.first_name,
+                    "last_name":user.last_name,
+                    "email":user.email
+                }
+            }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST )
+    
+class LoginView(APIView):
+    def post(self,request):
+        serializer = LoginSerializer(data=request.data)
+        
+        if serializer.is_valid():
+            email = serializer.validated_data['email']
+            password = serializer.validated_data['password']
+            
+            user = authenticate(request,email=email, password=password)
+            
+            if user is not None:
+                refresh = RefreshToken.for_user(user)
+                return Response({
+                    "refresh": str(refresh),
+                    "access": str(refresh.access_token),
+                    "user":{
+                        "id":user.id,
+                        "first_name": user.first_name,
+                        "last_name":user.last_name,
+                        "email":user.email
+                    }
+                }, status=status.HTTP_200_OK)
+            
+            return Response({"error": "Invalid email or password"}, status=status.HTTP_401_UNAUTHORIZED)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
